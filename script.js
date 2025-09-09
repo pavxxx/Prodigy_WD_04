dd87544b2cfba3953a02b5407a787f8d8d0fc6ad
pavxxx <pavithra020906@gmail.com>
2025-09-09 23:06:05 +0530

    draft commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
